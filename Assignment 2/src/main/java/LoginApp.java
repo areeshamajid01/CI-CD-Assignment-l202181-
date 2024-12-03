bf51f1671d1d7c41c66e2dc1c1a36555bb35756d
areeshamajid01 <l202181@lhr.nu.edu.pk>
2024-12-03 16:54:24 +0500

    solved yml errors

--- Assignment 2/src/main/java/LoginApp.java
+++ Assignment 2/src/main/java/LoginApp.java
@@ -81,6 +81,7 @@ public class LoginApp extends JFrame {
             System.out.println("test2");
             System.out.println("test3");
             System.out.println("test4");
+            System.out.println("test5");
             loginApp.setVisible(true);
         });
     }
